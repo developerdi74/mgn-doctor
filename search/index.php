ad3cdc3574204dfe76f505aa737a46a37a5b1987
Vladimir <developer@di74.ru>
2023-03-01 11:55:33 +0300

    поиск

--- search/index.php
+++ search/index.php
@@ -4,12 +4,15 @@ $APPLICATION->SetTitle("Поиск");
 ?>
 <div id="page" class="site page-grey page-dark-grey">
 	<div class="container">
-		<? $APPLICATION->IncludeComponent("bitrix:search.page", "doctor", Array(
-	"RESTART" => "Y",	// Искать без учета морфологии (при отсутствии результата поиска)
-		"CHECK_DATES" => "N",	// Искать только в активных по дате документах
-		"USE_TITLE_RANK" => "N",	// При ранжировании результата учитывать заголовки
-		"DEFAULT_SORT" => "rank",	// Сортировка по умолчанию
-		"arrFILTER" => array(	// Ограничение области поиска
+		<? $APPLICATION->IncludeComponent(
+	"bitrix:search.page", 
+	"doctor", 
+	array(
+		"RESTART" => "Y",
+		"CHECK_DATES" => "N",
+		"USE_TITLE_RANK" => "N",
+		"DEFAULT_SORT" => "rank",
+		"arrFILTER" => array(
 			0 => "iblock_mgn_doctor_content",
 			1 => "iblock_mgn_doctor_service",
 		),
@@ -23,39 +26,39 @@ $APPLICATION->SetTitle("Поиск");
 		"arrFILTER_iblock_catalog" => array(
 			0 => "all",
 		),
-		"SHOW_WHERE" => "N",	// Показывать выпадающий список "Где искать"
-		"SHOW_WHEN" => "N",	// Показывать фильтр по датам
-		"PAGE_RESULT_COUNT" => "25",	// Количество результатов на странице
-		"AJAX_MODE" => "N",	// Включить режим AJAX
+		"SHOW_WHERE" => "N",
+		"SHOW_WHEN" => "N",
+		"PAGE_RESULT_COUNT" => "25",
+		"AJAX_MODE" => "N",
 		"AJAX_OPTION_SHADOW" => "Y",
-		"AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
-		"AJAX_OPTION_STYLE" => "Y",	// Включить подгрузку стилей
-		"AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
-		"CACHE_TYPE" => "A",	// Тип кеширования
-		"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
-		"DISPLAY_TOP_PAGER" => "N",	// Выводить над результатами
-		"DISPLAY_BOTTOM_PAGER" => "Y",	// Выводить под результатами
-		"PAGER_TITLE" => "Результаты поиска",	// Название результатов поиска
-		"PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
-		"PAGER_TEMPLATE" => "arrows",	// Название шаблона
-		"USE_SUGGEST" => "N",	// Показывать подсказку с поисковыми фразами
-		"SHOW_ITEM_TAGS" => "N",	// Показывать теги документа
-		"SHOW_ITEM_DATE_CHANGE" => "N",	// Показывать дату изменения документа
-		"SHOW_ORDER_BY" => "N",	// Показывать сортировку
-		"SHOW_TAGS_CLOUD" => "N",	// Показывать облако тегов
-		"AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
-		"COMPONENT_TEMPLATE" => "clear",
-		"NO_WORD_LOGIC" => "Y",	// Отключить обработку слов как логических операторов
-		"FILTER_NAME" => "",	// Дополнительный фильтр
-		"USE_LANGUAGE_GUESS" => "N",	// Включить автоопределение раскладки клавиатуры
-		"SHOW_RATING" => "",	// Включить рейтинг
-		"RATING_TYPE" => "",	// Вид кнопок рейтинга
-		"PATH_TO_USER_PROFILE" => "",	// Шаблон пути к профилю пользователя
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AJAX_OPTION_HISTORY" => "N",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "36000000",
+		"DISPLAY_TOP_PAGER" => "N",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"PAGER_TITLE" => "Результаты поиска",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => "",
+		"USE_SUGGEST" => "Y",
+		"SHOW_ITEM_TAGS" => "Y",
+		"SHOW_ITEM_DATE_CHANGE" => "N",
+		"SHOW_ORDER_BY" => "N",
+		"SHOW_TAGS_CLOUD" => "Y",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"COMPONENT_TEMPLATE" => "doctor",
+		"NO_WORD_LOGIC" => "Y",
+		"FILTER_NAME" => "",
+		"USE_LANGUAGE_GUESS" => "N",
+		"SHOW_RATING" => "",
+		"RATING_TYPE" => "",
+		"PATH_TO_USER_PROFILE" => "",
 		"TAGS_SORT" => "NAME",
 		"TAGS_PAGE_ELEMENTS" => "150",
 		"TAGS_PERIOD" => "",
 		"TAGS_URL_SEARCH" => "",
-		"TAGS_INHERIT" => "Y",
+		"TAGS_INHERIT" => "N",
 		"FONT_MAX" => "50",
 		"FONT_MIN" => "10",
 		"COLOR_NEW" => "000000",
@@ -67,11 +70,18 @@ $APPLICATION->SetTitle("Поиск");
 		"STRUCTURE_FILTER" => "structure",
 		"NAME_TEMPLATE" => "",
 		"SHOW_LOGIN" => "Y",
-		"arrFILTER_iblock_mgn_doctor_content" => array(	// Искать в информационных блоках типа "iblock_mgn_doctor_content"
-			0 => "all",
+		"arrFILTER_iblock_mgn_doctor_content" => array(
+			0 => "27",
+			1 => "28",
+			2 => "32",
 		),
-		"arrFILTER_iblock_mgn_doctor_service" => array(	// Искать в информационных блоках типа "iblock_mgn_doctor_service"
-			0 => "all",
+		"arrFILTER_iblock_mgn_doctor_service" => array(
+			0 => "24",
+			1 => "25",
+		),
+		"arrWHERE" => array(
+			0 => "iblock_mgn_doctor_content",
+			1 => "iblock_mgn_doctor_service",
 		)
 	),
 	false
