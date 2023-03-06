b8407b022efe46d504e34c327823136abf0f74c6
Vladimir <developer@di74.ru>
2023-03-06 12:47:15 +0300

    Перенос метрики обертка чата в событие скролл

--- local/templates/mgn-doctor/header.php
+++ local/templates/mgn-doctor/header.php
@@ -23,6 +23,26 @@
 
   <link rel="icon" href="https://mgn-doctor.ru/favicon.svg" type="image/svg+xml">
 
+		  <!— Yandex.Metrika counter —>
+		<script type="text/javascript">
+			(function(m, e, t, r, i, k, a){
+				m[i]=m[i]||function(){
+					(m[i].a=m[i].a||[]).push(arguments)
+				};
+				m[i].l=1*new Date();
+				k=e.createElement(t), a=e.getElementsByTagName(t)[0], k.async=1, k.src=r, a.parentNode.insertBefore(k, a)
+			})
+			(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+			ym(87764265, "init", {
+				clickmap:true,
+				trackLinks:true,
+				accurateTrackBounce:true,
+				webvisor:true,
+				ecommerce:"dataLayer"
+			});
+		</script>
+		  <noscript><div><img src="https://mc.yandex.ru/watch/87764265" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+		  <!— /Yandex.Metrika counter —>
   <!-- libraries -->
   <?
   Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/js/jquery.min.js');
