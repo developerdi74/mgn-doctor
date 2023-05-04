c981eb094c25b1824036de27cd0a1336cd67895a
Vladimir <developer@di74.ru>
2023-05-04 11:38:54 +0300

    test

--- detskie-vrachi/index.php
+++ detskie-vrachi/index.php
@@ -1,6 +1,6 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Врачи в медицинском центре “Семейный доктор”");
+$APPLICATION->SetTitle("Детские врачи в медицинском центре “Семейный доктор”");
 
 $APPLICATION->AddChainItem("Детские врачи", "/detskie-vrachi/");
 ?><section class="specialists-inner all-our-specialists">
