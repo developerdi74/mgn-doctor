d10a68a461b09912a95befa1f464568ea698d71d
Vladimir <developer@di74.ru>
2023-05-18 06:32:07 +0300

    acceess feedback

--- feedback/.access.php
+++ feedback/.access.php
@@ -3,7 +3,7 @@ $PERM["obratnaya-svyaz"]["2"]="D";
 $PERM["obratnaya-svyaz"]["5"]="D";
 $PERM["obratnaya-svyaz"]["3"]="D";
 $PERM["obratnaya-svyaz"]["4"]="D";
-$PERM["obratnaya-svyaz"]["8"]="X";
-$PERM["obratnaya-svyaz"]["6"]="X";
-$PERM["obratnaya-svyaz"]["7"]="X";
+$PERM["obratnaya-svyaz"]["8"]="R";
+$PERM["obratnaya-svyaz"]["6"]="R";
+$PERM["obratnaya-svyaz"]["7"]="R";
 ?>
