df04da34411a03aff64d5099aa5ee9fef8919d89
Vladimir <developer@di74.ru>
2023-11-07 09:18:21 +0300

    вклчил кеш

--- include/api/loadListPlannings.php
+++ include/api/loadListPlannings.php
@@ -1,6 +1,7 @@
 <?php
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php');
-$reset = 1;
+
+$reset = 0; // 1 - сброк кеша
 echo (dataCache('cacheSpecialists-'.$_GET['cache_code'], function (){
     return json_encode(getPlannings($_GET));
 }, 12*60*60, 'sectionListDocs', $reset));
