05ed4892d605de97e2ff00d00823ffcfa6761b47
Vladimir <developer@di74.ru>
2023-12-06 14:29:32 +0300

    форма загрузки анкет

--- feedback/nut/anketa/index.php
+++ feedback/nut/anketa/index.php
@@ -3,7 +3,25 @@ require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
 $APPLICATION->SetPageProperty("title", "Семейный доктор в Магнитогорске. Загрузить анкету");
 $APPLICATION->SetTitle("Семейный доктор в Магнитогорске. Загрузить анкету");
 ?>
-
+<div class="container">
+    <h3>Анкеты для скачивания и заполнения перед приемом нутрициолога</h3>
+    <p class="">Перед приемом нутрициолога, обязательно, скачайте, заполните и отправьте удобным способом доктору указанные ниже анкеты</p>
+    <ul>
+        <li><a target="_blank" href="/upload/ankets/Дневник питания и образа жизни.docx">Дневник питания и образа жизни.docx</a></li>
+        <li><a target="_blank" href="/upload/ankets/Опросник пациента.docx">Опросник пациента.docx</a></li>
+        <li><a target="_blank" href="/upload/ankets/Опросник по ключевым дефицитам верный.xlsx">Опросник по ключевым дефицитам верный.xlsx</a></li>
+    </ul>
+        <p>
+            После скачивания:<br>
+            <ul>
+                <li> Заполните анкеты в электронном виде <b>или</b> распечатайте, заполните от руки и сделайте снимки</li>
+                <li> Фотографии или анкеты в электронном виде отправьте одним из способов ниже:<br>
+                    - по электронной почте, приложив в письме файлы, по адресу <a href="mailto:nutriciolog@doctor-74.ru">nutriciolog@doctor-74.ru</a> <br>
+                    - воспользоваться <a href="#form-load-ankets">формой отправки</a> ниже, выбрав файлы <br><br><br>
+                </li>
+            </ul>
+        </p>
+</div>
 <?$APPLICATION->IncludeComponent(
 	"bitrix:form.result.new", 
 	"loadankets", 
